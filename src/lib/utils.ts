83c06e84f7a15b360dbb6bb487191316b556bace
Kuro32183 <92940109+Kuro32183@users.noreply.github.com>
2026-05-03 01:07:55 +0900

    feat: add lib/utils (cn, formatPrice)

---
+++ src/lib/utils.ts
@@ -0,0 +1,16 @@
+// src/lib/utils.ts
+
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function formatPrice(price: number): string {
+  return new Intl.NumberFormat('ja-JP', {
+    style: 'currency',
+    currency: 'JPY',
+    minimumFractionDigits: 0,
+  }).format(price);
+}
